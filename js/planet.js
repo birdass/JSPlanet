2178b1db6c512ccc99ac2df48e17e69058b1335b
Opilane TTHK <opilane@tthk.ee>
2020-12-03 09:48:34 +0200

    commit

--- js/planet.js
+++ js/planet.js
@@ -1,20 +1,10 @@
 let planet = ["Mercury", "Venus","Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
-planet.push('Pluto');
-console.log(planet);
+planet.push('Pluto'); //  добавление планеты в массив
+console.log(planet); // показать длинну массива
+let output = '';
 
-
-const html = `  
-    <ul> 
-        <li> ${planet[0]} </il>
-        <li> ${planet[1]} </il>
-        <li> ${planet[2]} </il>
-        <li> ${planet[3]} </il>
-        <li> ${planet[4]} </il>
-        <li> ${planet[5]} </il>
-        <li> ${planet[6]} </il>
-        <li> ${planet[7]} </il>
-        <li> ${planet[8]} </il>
-    </ul>
-    `;
-
-    document.body.innerHTML = html;
+for(let i = 0; i < planet.length; i++){
+    console.log(i+1, planet[i]);
+    output += ` <li>${planet[i]}</il>` ;
+}
+document.querySelector(".list"). innerHTML = output;
